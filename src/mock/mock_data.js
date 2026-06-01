ace1d06b4ab034f323cfc02b4eeb237e4c4af61a
nbwzlyd <420907013@qq.com>
2026-06-01 15:37:42 +0800

    chore: 更新导航数据 - 2026/6/1 15:37:54

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -7,11 +7,18 @@ export const mockData = {
       "order": 0,
       "sites": [
         {
-          "id": "site-1778929068955",
-          "name": "猫图 AI",
-          "url": "https://catsapi.com",
-          "description": "AI 生图网站，Image2 3 分/张,每日有免费额度，",
-          "icon": "https://www.faviconextractor.com/favicon/catsapi.com"
+          "id": "site-1779977193929",
+          "name": "图片编辑",
+          "url": "https://www.iloveimg.com/zh-cn",
+          "description": "免费无广图片编辑网站",
+          "icon": "https://www.faviconextractor.com/favicon/www.iloveimg.com"
+        },
+        {
+          "id": "site-1780298800750",
+          "name": "微信文章下载",
+          "url": "https://changfengbox.top/wechat",
+          "description": "",
+          "icon": ""
         },
         {
           "id": "site-1752649007053",
@@ -19,6 +26,20 @@ export const mockData = {
           "url": "https://www.bejson.com/",
           "description": "工具大全",
           "icon": "/sitelogo/www.bejson.com.ico"
+        },
+        {
+          "id": "github",
+          "name": "GitHub",
+          "url": "https://github.com",
+          "description": "代码托管平台",
+          "icon": "/sitelogo/github.com.ico"
+        },
+        {
+          "id": "site-1778929068955",
+          "name": "猫图 AI",
+          "url": "https://catsapi.com",
+          "description": "AI 生图网站，Image2 3 分/张,每日有免费额度，",
+          "icon": "https://www.faviconextractor.com/favicon/catsapi.com"
         },
         {
           "id": "curlconverter",
@@ -40,20 +61,6 @@ export const mockData = {
           "url": "https://chatgpt.com",
           "description": "OpenAI,好用👌",
           "icon": "/sitelogo/chatgpt.com.ico"
-        },
-        {
-          "id": "github",
-          "name": "GitHub",
-          "url": "https://github.com",
-          "description": "代码托管平台",
-          "icon": "/sitelogo/github.com.ico"
-        },
-        {
-          "id": "site-1779977193929",
-          "name": "图片编辑",
-          "url": "https://www.iloveimg.com/zh-cn",
-          "description": "免费无广图片编辑网站",
-          "icon": "https://www.faviconextractor.com/favicon/www.iloveimg.com"
         }
       ]
     },
